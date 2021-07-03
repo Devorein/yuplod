49067a0384fa86dc0231c6d02d020148093d941d
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 18:58:01 +0600

    Added prettier config file

--- backend/src/controllers/posts.ts
+++ backend/src/controllers/posts.ts
@@ -1,32 +1,35 @@
-import { Request, Response } from "express";
-import { IPostUpdate } from "src/types";
-import { Post } from "../models";
+import { Request, Response } from 'express';
+import { IPostUpdate } from 'src/types';
+import { Post } from '../models';
 
-export async function getAllPosts(_: Request, res: Response){
-  try{
+export async function getAllPosts(_: Request, res: Response) {
+  try {
     const posts = await Post.getAll();
     res.status(200).json({ data: posts, status: 'success' });
-  } catch(err){
+  } catch (err) {
     res.status(404).json({ message: [err.message], status: 'error' });
   }
 }
 
-export async function getPostById(req: Request, res: Response){
-  try{
-    const {id} = req.params;
+export async function getPostById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
     const posts = await Post.getById(id);
     res.status(200).json({ data: posts, status: 'success' });
-  } catch(err){
+  } catch (err) {
     res.status(404).json({ message: [err.message], status: 'error' });
   }
 }
 
-export async function updatePost(req: Request<{data: IPostUpdate}>, res: Response){
+export async function updatePost(
+  req: Request<{ data: IPostUpdate }>,
+  res: Response
+) {
   try {
-    const {data} = req.params;
+    const { data } = req.params;
     const post = await Post.update(data);
-    res.status(200).json({data: post, status: 'success'})
-  }catch(err){
+    res.status(200).json({ data: post, status: 'success' });
+  } catch (err) {
     res.status(404).json({ message: [err.message], status: 'error' });
   }
 }
