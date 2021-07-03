be0d2e41535bbbad453e8b4c6cf951d46c389777
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 22:46:21 +0600

    B: Transferred createUser to Auth.register

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,14 +1,7 @@
 import { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 import { User } from '../models';
-import { IRequest, IUserCreate } from '../types';
-import {
-  checkFields,
-  createJsonErrorResponse,
-  createJsonSuccessResponse,
-  validateEmail,
-  validatePassword
-} from '../utils';
+import { IRequest } from '../types';
+import { createJsonErrorResponse, createJsonSuccessResponse } from '../utils';
 
 export async function getAllUsers(_: Request, res: Response) {
   try {
@@ -26,50 +19,6 @@ export async function getUserById(req: Request, res: Response) {
     createJsonSuccessResponse(res, users);
   } catch (err) {
     createJsonErrorResponse(res, [err.message]);
-  }
-}
-
-export async function createUser(
-  req: Request<any, any, { data: IUserCreate }>,
-  res: Response
-) {
-  const { data } = req.body;
-  const errorMessages = checkFields(data, [
-    [
-      'email',
-      (email: string) => {
-        const isValid = validateEmail(email);
-        if (isValid) return true;
-        else return 'Invalid email provided';
-      }
-    ],
-    'first_name',
-    'last_name',
-    [
-      'password',
-      (password: string) =>
-        validatePassword(password)
-          ? true
-          : 'Password must contain at least 1 lowercase, 1 uppercase, 1 digit and 8 characters'
-    ],
-    'username'
-  ]);
-  if (errorMessages.length !== 0) {
-    createJsonErrorResponse(res, errorMessages);
-  } else {
-    try {
-      const user = await User.create(data);
-      const token = jwt.sign(
-        { id: user.id },
-        process.env.JWT_SECRET as string,
-        {
-          expiresIn: process.env.JWT_EXPIRE
-        }
-      );
-      createJsonSuccessResponse(res, { user, token });
-    } catch (err) {
-      createJsonErrorResponse(res, [err.message]);
-    }
   }
 }
 
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,18 +1,10 @@
 import express from 'express';
-import {
-  createUser,
-  deleteUser,
-  getAllUsers,
-  getUserById
-} from '../controllers';
+import { deleteUser, getAllUsers, getUserById } from '../controllers';
 import { auth } from '../middlewares';
 
 const UsersRouter = express.Router();
 
-UsersRouter.route('/')
-  .get(getAllUsers)
-  .post(createUser)
-  .delete(auth, deleteUser);
+UsersRouter.route('/').get(getAllUsers).delete(auth, deleteUser);
 UsersRouter.route('/:id').get(getUserById);
 
 export default UsersRouter;
