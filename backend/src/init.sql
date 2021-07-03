d351224bf12a65260aa9de2bea9ad26797e74aad
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 17:50:59 +0600

    B: Added sql code to create users table

--- backend/src/init.sql
+++ backend/src/init.sql
@@ -1,9 +1,17 @@
 -- SQL Code to create posts table
 create table posts (
   id serial primary key,
-  user_id integer not null,
+  user_id integer references users(id) on delete cascade,
   image_url text,
   caption text,
   created_at timestamp,
   updated_at timestamp
 );
+
+-- SQL code to create users table
+create table users (
+  id serial primary key,
+  username varchar(255),
+  email varchar(255),
+  password varchar(255)
+)
--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllPosts } from "../controllers";
+import { getAllPosts, getPostById } from "../controllers";
 
 const PostsRouter = express.Router();
 
 PostsRouter.route("/").get(getAllPosts);
+PostsRouter.route("/:id").get(getPostById);
 
 export default PostsRouter;
