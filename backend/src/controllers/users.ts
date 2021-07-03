67ecbec568aa45b858bac47bffb864c1a9cc04d0
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 18:00:20 +0600

    B: Added get user by id controller

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -9,3 +9,13 @@ export async function getAllUsers(_: Request, res: Response){
     res.status(404).json({ message: err.message, status: 'success' });
   }
 }
+
+export async function getUserById(req: Request, res: Response){
+  try{
+    const id = req.params.id;
+    const {rows: users} = await pool.query(`SELECT * from users where id = $1`, [id]);
+    res.status(200).json({ data: users[0], status: 'success' });
+  } catch(err){
+    res.status(404).json({ message: err.message, status: 'success' });
+  }
+}
