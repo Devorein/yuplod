acbe62068dd020f5b2015d04c38ce0a75b10046d
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 17:45:31 +0600

    B: Added get post by id controller

--- backend/src/controllers/posts.ts
+++ backend/src/controllers/posts.ts
@@ -1,10 +1,20 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { IPost } from "../types";
 import { pool } from "../utils";
 
-export async function getAllPosts(_: any, res: Response){
+export async function getAllPosts(_: Request, res: Response){
   try{
     const {rows} = await pool.query<IPost>("SELECT * from posts");
+    res.status(200).json({ data: rows, status: 'success' });
+  } catch(err){
+    res.status(404).json({ message: err.message, status: 'success' });
+  }
+}
+
+export async function getPostById(req: Request, res: Response){
+  try{
+    const id = req.params.id;
+    const {rows} = await pool.query<IPost>(`SELECT * from posts where id = $1`, [id]);
     res.status(200).json({ data: rows, status: 'success' });
   } catch(err){
     res.status(404).json({ message: err.message, status: 'success' });
