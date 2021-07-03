aade04f9eddc72868d18c4612d10196c3007b5c9
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 17:37:21 +0600

    B: Added types for posts

--- backend/src/routes/posts.ts
+++ backend/src/routes/posts.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { IPost } from "../types";
 import { pool } from "../utils";
 
 const PostsRouter = express.Router();
 
 PostsRouter.route("/").get(async (_, res)=>{
-  const {rows} = await pool.query("SELECT * from posts");
+  const {rows} = await pool.query<IPost>("SELECT * from posts");
   res.status(200).json({ data: rows, status: 'success' });
 });
 
---
+++ backend/src/types.ts
@@ -0,0 +1,8 @@
+export interface IPost{
+  id: number
+  created_at: number
+  updated_at: number
+  image_url: string
+  caption: string
+  user_id: number
+}
