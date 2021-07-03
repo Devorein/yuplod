9744c34cd7506dace3ef7842098eb96c50c1f268
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 20:04:02 +0600

    B: Added getByEmail, getByUsername & getByUsernameOrEmail User model methods

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,14 +1,41 @@
-import { IUser } from "../types";
-import { pool } from "../utils";
+import { IUser } from '../types';
+import { pool } from '../utils';
 
 export default class User {
-  static async getAll(){
-    const {rows: users} = await pool.query<IUser>("SELECT * from users");
+  static async getAll() {
+    const { rows: users } = await pool.query<IUser>('SELECT * FROM users');
     return users;
   }
 
-  static async getById(id: string){
-    const {rows: users} = await pool.query<IUser>(`SELECT * from users where id = $1`, [id]);
+  static async getById(id: string) {
+    const { rows: users } = await pool.query<IUser>(
+      `SELECT * FROM users WHERE id = $1`,
+      [id]
+    );
+    return users;
+  }
+
+  static async getByEmail(email: string) {
+    const { rows: users } = await pool.query<IUser>(
+      `SELECT * FROM users WHERE email = $1`,
+      [email]
+    );
+    return users;
+  }
+
+  static async getByUsername(username: string) {
+    const { rows: users } = await pool.query<IUser>(
+      `SELECT * FROM users WHERE username = $1`,
+      [username]
+    );
+    return users;
+  }
+
+  static async getByUsernameOrEmail(email: string, username: string) {
+    const { rows: users } = await pool.query<IUser>(
+      `SELECT * FROM users WHERE username = $1 or email = $2`,
+      [username, email]
+    );
     return users;
   }
 }
