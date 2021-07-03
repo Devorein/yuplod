95ef388b747af2e7d78743d3ce9ee217a3ddc94e
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 20:25:02 +0600

    B: Added callback tuple support for checkFields

--- backend/src/utils/checkFields.ts
+++ backend/src/utils/checkFields.ts
@@ -1,8 +1,21 @@
-export function checkFields<T>(data: T, fields: (keyof T)[]) {
+export function checkFields<T>(
+  data: T,
+  fields: (keyof T | [keyof T, (data: any) => string | undefined])[]
+) {
   const messages: string[] = [];
   fields.forEach((field) => {
-    if (data[field] === undefined || data[field] === null) {
-      messages.push(`field:${field} is required`);
+    if (typeof field === 'string') {
+      if (data[field] === undefined || data[field] === null) {
+        messages.push(`field:${field} is required`);
+      }
+    } else if (Array.isArray(field)) {
+      const [fieldToCheck, callback] = field;
+      if (data[fieldToCheck] !== undefined && data[fieldToCheck] !== null) {
+        const message = callback(data[fieldToCheck]);
+        if (message) messages.push(message);
+      } else {
+        messages.push(`field:${fieldToCheck} is required`);
+      }
     }
   });
   return messages;
