218a27892bcdbd393fc742af2257efd47eb21968
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 18:47:24 +0600

    B: Integrated Post model class in controllers

--- backend/src/controllers/posts.ts
+++ backend/src/controllers/posts.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
-import { IPost } from "../types";
-import { pool } from "../utils";
+import { Post } from "../models";
 
 export async function getAllPosts(_: Request, res: Response){
   try{
-    const {rows: posts} = await pool.query<IPost>("SELECT * from posts");
+    const posts = await Post.getAll();
     res.status(200).json({ data: posts, status: 'success' });
   } catch(err){
     res.status(404).json({ message: [err.message], status: 'error' });
@@ -13,8 +12,8 @@ export async function getAllPosts(_: Request, res: Response){
 
 export async function getPostById(req: Request, res: Response){
   try{
-    const id = req.params.id;
-    const {rows: posts} = await pool.query<IPost>(`SELECT * from posts where id = $1`, [id]);
+    const {id} = req.params;
+    const posts = await Post.getById(id);
     res.status(200).json({ data: posts, status: 'success' });
   } catch(err){
     res.status(404).json({ message: [err.message], status: 'error' });
