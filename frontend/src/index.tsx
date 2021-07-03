cef7cd5f3898c912a7639f0f694feac74d3f83a9
Safwan Shaheer <devorein00@gmail.com>
2021-07-03 23:39:24 +0600

    Craeted react driver index code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,23 @@
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
-import './index.css';
+import './index.scss';
+import { generateTheme } from './utils';
+
+const Root = () => {
+  const generatedTheme = generateTheme();
+  return <ThemeProvider theme={generatedTheme}>
+    <App />
+  </ThemeProvider>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Root />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
