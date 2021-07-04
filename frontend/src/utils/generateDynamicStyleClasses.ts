dbb4c026f46dfb90a8b6d2fb4523a8559224a363
Safwan Shaheer <devorein00@gmail.com>
2021-07-04 08:10:49 +0600

    Added generateDynamicStyleClasses util module

---
+++ frontend/src/utils/generateDynamicStyleClasses.ts
@@ -0,0 +1,36 @@
+import { makeStyles } from '@material-ui/core';
+import { ExtendedTheme } from '../types';
+
+export function generateDynamicStyleClasses() {
+  return makeStyles((theme: ExtendedTheme) => ({
+    root: {
+      '& ::-webkit-scrollbar': {
+        width: 10
+      },
+      '& ::-webkit-scrollbar-track': {
+        backgroundColor: theme.color.dark
+      },
+      '& ::-webkit-scrollbar-thumb': {
+        backgroundColor: theme.color.light
+      },
+      '& .bg-dark': {
+        background: theme.color.dark
+      },
+      '& .bg-base': {
+        background: theme.color.base
+      },
+      '& .bg-light': {
+        background: theme.color.light
+      },
+      '& .bg-opposite_dark': {
+        background: theme.color.opposite_dark
+      },
+      '& .bg-opposite_base': {
+        background: theme.color.opposite_base
+      },
+      '& .bg-opposite_light': {
+        background: theme.color.opposite_light
+      }
+    }
+  }))();
+}
--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,1 +1,2 @@
+export * from './generateDynamicStyleClasses';
 export * from './generateTheme';
