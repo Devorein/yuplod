8c0e0d55d91b93b044c98336134c0ecfd65bfb05
Safwan Shaheer <devorein00@gmail.com>
2021-07-04 16:49:27 +0600

    F: Auto fetch user on first page load

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,13 +1,30 @@
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
+import axios from "axios";
 import React, { useContext } from 'react';
+import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 import { RootContext } from '../../contexts';
+import { IUser } from '../../types';
 
 export default function Navbar() {
   const { currentUser, setCurrentUser } = useContext(RootContext);
   const isAuth = Boolean(currentUser);
+
+  useQuery('me', async () => {
+    const token = localStorage.getItem('yuplod.token');
+    if (token) {
+      const { data } = await axios.get<{ data: IUser }>(`http://localhost:4000/api/v1/users/me`, {
+        headers: {
+          authorization: `Bearer ${token}`
+        }
+      })
+      if (data.data) {
+        setCurrentUser(data.data)
+      }
+    }
+  })
 
   function render() {
     if (!isAuth) {
